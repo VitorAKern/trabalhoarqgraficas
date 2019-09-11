1848fb74fbcbd65c463877efe1f8b67e7d364428
Vitor <vitor_kern@yahoo.com.br>
2019-09-10 21:54:44 -0300

    Testes de Leitura

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ let Height = 8;
 var memory = new Array(Width * Height);
 var debug = true;
 
+const command = {
+    LD: 'LD',
+    LD2: 'LD2',
+    LD3: 'LD3',
+    DB: 'DB'
+}
+
 function render() {
     let html = '<table cellpadding=0 cellspacing=0>'
   
@@ -27,8 +34,33 @@ function render() {
 
   function execute(){
     var lines = document.getElementById('Code').value.split('\n');
+    let aux;
+    var position;
+    var stopCond;
     for(var i = 0;i < lines.length;i++){
+        let aux = lines[i];
+        for(let j = 0; j < lines[i].length; i++){
+            if(stopCond == true){
+                if(aux[j] == " "){
+                    stopCond = false;
+                    break;
+                }
+                position += aux[j];
+            }
+            if(aux[j] == "#"){
+                stopCond = true;
+                position = aux[j+1] + aux[j+2];
+            }
+        }
+        commands = lines[i].substring(1,3);
+
+        switch(commands){
+            case commands.DB:
+            document.getElementById(position).value = "50"
+
+        }
         //code here using lines[i] which will give you each line
     }
   }
+
   this.render();
